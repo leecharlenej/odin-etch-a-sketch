ff80a5ab0a7a4e4d09522bcf1db73fe750036d3a
leecharlenej <lee.charlenej@gmail.com>
2024-08-15 00:00:38 +0800

    Change method to add grid squares. Pass default input of 4.

--- javascript.js
+++ javascript.js
@@ -3,12 +3,17 @@
 // ----------------------------------
 let sketchpad = document.querySelector(".sketchpad");
 
-for (let i=0; i<16; i++) {
-
+for (let i=0; i<4; i++) {
     const div = document.createElement("div");
-    div.classList.add("grid");
-    sketchpad.appendChild(div);
+    div.classList.add("row");
 
+    for (let j=0; j<4; j++) {
+        const row = document.createElement("div");
+        row.classList.add('grid');
+        div.appendChild(row);
+    }
+
+    sketchpad.appendChild(div);
 }
 
 // ----------------------------------
