53b523436316e372d89afad3a87fbe87ad6c62af
leecharlenej <lee.charlenej@gmail.com>
2024-08-15 11:37:05 +0800

    Fix changeSize function.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-etch-a-sketch
+## Reflections
+1. Tried to use flex-wrap and flex-basis but flex items kept overflowing the container.
+- Used loops; added rows and squares per row.
--- javascript.js
+++ javascript.js
@@ -1,25 +1,7 @@
 // ----------------------------------
-// Create responsive square grid
-// ----------------------------------
-let sketchpad = document.querySelector(".sketchpad");
-
-for (let i=0; i<4; i++) {
-    const div = document.createElement("div");
-    div.classList.add("row");
-
-    for (let j=0; j<4; j++) {
-        const row = document.createElement("div");
-        row.classList.add('grid');
-        div.appendChild(row);
-    }
-
-    sketchpad.appendChild(div);
-}
-
-// ----------------------------------
 // Create hover effect
 // ----------------------------------
-let gridList = document.querySelectorAll(".grid");
+// let gridList = document.querySelectorAll(".grid");
 
 const handleMouseEnter = (event) => {
     event.target.style.backgroundColor = 'rgb(97, 151, 223)';
@@ -31,32 +13,40 @@ const handleMouseLeave = (event) => {
     //event.target.setAttribute("style", "background-color: black;");
 };
 
-for (let i=0; i<gridList.length; i++){
-    gridList[i].addEventListener("mouseenter", handleMouseEnter);
-    gridList[i].addEventListener("mouseleave", handleMouseLeave);
+// ----------------------------------
+// Create responsive square grid
+// ----------------------------------
+
+function addSquaresPerRow (numPerRow, elem) {
+
+    for (let i=0; i<numPerRow; i++) {
+        const square = document.createElement("div");
+        square.classList.add('square');
+        square.addEventListener("mouseenter", handleMouseEnter);
+        square.addEventListener("mouseleave", handleMouseLeave);
+        elem.appendChild(square);
+    };
 }
 
-// ----------------------------------
-// Reset
-// ----------------------------------
-let reset = document.querySelector("#resetButton")
+let sketchpad = document.querySelector(".sketchpad");
 
-const resetColours = (event => {
-    console.log("welps");
+for (let i=0; i<4; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
 
-});
+    addSquaresPerRow(4,row);
 
-resetButton.addEventListener("click", resetColours);
+    sketchpad.appendChild(row);
+}
 
 // ----------------------------------
 // Change size
 // ----------------------------------
 let sizeButton = document.querySelector("#sizeButton")
 
-const changeSize = (event => {
-
-    let blankSketchpad = document.querySelector(".sketchpad");
-    let squaresTotalNum;
+function changeSize (event) {
+    console.log(event);
+    sketchpad.innerHTML ="";
 
     while (true) {
         userInput = parseInt(prompt("No. of squares per side?"));
@@ -66,29 +56,38 @@ const changeSize = (event => {
         };
     };
 
+    for (let i=0; i<userInput; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+    
+        addSquaresPerRow(userInput,row);
 
-
-    flexNum = Math.floor(100/userInput)-2;
-    squaresTotalNum = userInput**2;
-    console.log(`Total squares: ${squaresTotalNum}`);
-    blankSketchpad.innerHTML ="";
-
-    for (let i=0; i<squaresTotalNum; i++) {
-        const div = document.createElement("div");
-        div.classList.add("grid");
-        
-
-        div.addEventListener("mouseenter", handleMouseEnter);
-        div.addEventListener("mouseleave", handleMouseLeave);
-
-        div.style.flex = `1 1 ${flexNum}%`;
-        blankSketchpad.appendChild(div);
-
-        
-        console.log(div);
-    }
-});
+        sketchpad.appendChild(row);
+    };
+}
 
 sizeButton.addEventListener("click", changeSize);
+
+// ----------------------------------
+// Reset
+// ----------------------------------
+let resetButton = document.querySelector("#resetButton");
+
+function resetColours () {
+    let squaresList = document.querySelectorAll(".square");
+    console.log(squaresList);
+
+    for (let i=0; i<squaresList.length; i++){
+        squaresList[i].style.backgroundColor='';
+
+    };
+}
+
+resetButton.addEventListener("click", resetColours);
+
+// ----------------------------------
+// Randomize colours
+// ----------------------------------
+
 
 
